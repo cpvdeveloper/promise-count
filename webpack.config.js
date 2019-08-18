a2a82e141721cc34b21cfa56f051998c05f0590e
Chris Vibert <chris.vibert@gousto.co.uk>
2019-08-18 16:00:35 +0100

    Add build

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|lib)/,
+        use: {
+          loader: 'babel-loader',
+        }
+      }
+    ]
+  },
+};
